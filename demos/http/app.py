526f4c08928f1a75a701770fe9b05ae3cba54bd1
franck <franck_gxu@outlook.com>
2020-08-09 22:58:23 +0200

    test

--- demos/http/app.py
+++ demos/http/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask,request
+from flask import Flask,request,redirect,abort
 
 app = Flask(__name__)
 
@@ -18,3 +18,31 @@ def hi():
 @app.route('/goback/<int:year>')
 def go_back(year):
     return '<p>Welcome to %d</p>'%(2020-year)
+
+
+@app.route('/colors/<any(blue,white,red):color>')
+def three_color(color):
+    return '<p>Love is paitent and kind,Love is not jealous or boastful or proud of rude.</p>'
+
+
+@app.before_request
+def do_something():
+    print('test')
+
+
+
+# response 
+@app.route('/resp')
+def resp():
+    return '<h1>Hello,Flask</h1>',201
+
+# redirect 
+@app.route('/red')
+def myred():
+    return redirect('https:google.com')
+
+
+# error response
+@app.route('/404')
+def not_found():
+    abort(status=404)
